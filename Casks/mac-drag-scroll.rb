a571bf9b6cd5abfeca980b8c0d67e6089ee2f47c
Martin Calander <martin.calander@gmail.com>
2026-07-09 13:18:42 +0200

    Add Mac Drag Scroll cask

---
+++ Casks/mac-drag-scroll.rb
@@ -0,0 +1,16 @@
+cask "mac-drag-scroll" do
+  version "1.0.2"
+  sha256 :no_check
+
+  url "https://github.com/martincalander/MacDragScroll/releases/download/v#{version}/MacDragScroll.zip"
+  name "Mac Drag Scroll"
+  desc "Windows-style drag scrolling for external mice"
+  homepage "https://github.com/martincalander/MacDragScroll"
+
+  auto_updates true
+  depends_on macos: :tahoe
+
+  app "Mac Drag Scroll.app"
+
+  uninstall quit: "com.martincalander.macdragscroll"
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Martin Calander Homebrew Tap
+
+Homebrew tap for [Mac Drag Scroll](https://github.com/martincalander/MacDragScroll).
+
+## Install
+
+```sh
+brew install --cask martincalander/tap/mac-drag-scroll
+```
+
+Mac Drag Scroll is currently unsigned and not Apple-notarized. On first launch, macOS may require opening it from Finder with right-click **Open**.
+
+## Update
+
+```sh
+brew update
+brew upgrade --cask mac-drag-scroll
+```
