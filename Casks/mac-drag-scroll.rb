b3f757d434131cdf098447b22bec04651efa84be
Martin Calander <martin.calander@gmail.com>
2026-07-10 23:56:14 +0200

    Update Mac Drag Scroll to 1.1.0

--- Casks/mac-drag-scroll.rb
+++ Casks/mac-drag-scroll.rb
@@ -1,6 +1,6 @@
 cask "mac-drag-scroll" do
-  version "1.0.7"
-  sha256 "140757c1bb5db0ede2c48e99033528de59e912ce8df830850175bdfabd99bb96"
+  version "1.1.0"
+  sha256 "fdf2c0753cf6ac60de5f396b76ffa2cd404821c9b8e6233fb18d736c12cf4816"
 
   url "https://github.com/martincalander/MacDragScroll/releases/download/v#{version}/MacDragScroll.zip"
   name "Mac Drag Scroll"
@@ -8,7 +8,7 @@ cask "mac-drag-scroll" do
   homepage "https://github.com/martincalander/MacDragScroll"
 
   auto_updates true
-  depends_on macos: :tahoe
+  depends_on macos: :sonoma
 
   app "Mac Drag Scroll.app"
 
